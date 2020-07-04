cefe8e762d938eb8162a55ed5197b85d0bdebc92
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2020-07-04 15:12:31 +0530

    Post model

---
+++ models/Post.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Post = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    default: 'no-image.jpg',
+  },
+  slug: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Post', Post);
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Route files
-const posts = require('./routes/posts');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to db
+connectDB();
+
+// Route files
+const posts = require('./routes/posts');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -21,3 +25,11 @@ app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
